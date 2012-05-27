d6fdfd338c09da9cede37f7403be8ccda8f2f88e
Eric Maierson <eric@ericmaierson.com>
2012-05-26 23:00:51 -0400

    sentence and paragraph function
    put sentence and single_graph in to functions. They work. working on main to get user input on number

--- derpum_lipsum.py
+++ derpum_lipsum.py
@@ -2,24 +2,62 @@
 import random
 
 #create dictionary 
-derpy_list = ['derpy', 'derp', 'herp-derp', 'bederp', 'derpy', 'herp']
+derpy_list = ['derpy', 'derp', 'herp-derp', 'bederp', 'derpy', 'herp', 'herpy']
 
-i = 1
 
-firstword = random.choice(derpy_list)
-cap = firstword.capitalize()
-print cap,
+def main():
+	ask = raw_input("sentence [s] or paragraph [p] > " )
 
-i = 1
+	if ask[0].lower() == "s":
+		sentence()
+	elif ask[0].lower() == "p":
+		number_of_graphs == raw_input("How many paragraphs?")
+		print number_of_graphs
+		'''
+		if number_of_graphs == 1
+			single_paragraph():
+				else:
+					main()
+		'''
+	else:
+		print "hi"
 
-while i < 5:
+def sentence():
+
+	#selects first word from dictionary
+	firstword = random.choice(derpy_list)
 	
-	middle = random.choice(derpy_list)
+	# capitalizes first word
+	cap = firstword.capitalize()
+	print cap,
+
+	#set random sentence length
+	sentence_length = random.randint(1, 10)
+
+	i = 1
+	for i in range(sentence_length):
 		
-	print middle,
+		middle = random.choice(derpy_list)
+		print middle,
 		
+		i = i + 1
+	
+	#selects last word in sentence and adds period
+	lastword = random.choice(derpy_list)
+	print lastword + ".",
+
+
+def single_paragraph():
+
+	#set random sentence length
+	paragraph_length = random.randint(3, 6)
+	
+	i = 1
+	for i in range(paragraph_length):
+		
+		sentence()
+					
 	i = i + 1
-	
 
-lastword = random.choice(derpy_list)
-print lastword + "."
+	
+main()
